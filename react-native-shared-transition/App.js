adae98bd9aecee1399f604c2d26278f609f23959
Matheus Apolonio <matheus.apolonio@hotmail.com>
2019-06-06 01:09:12 -0300

    Animating image to fill top of the screen

--- react-native-shared-transition/App.js
+++ react-native-shared-transition/App.js
@@ -11,8 +11,8 @@ import {
   SafeAreaView
 } from "react-native";
 
-let SCREEN_WIDTH = Dimensions.get('window').width;
-let SCREEN_HEIGHT = Dimensions.get('window').height;
+let SCREEN_WIDTH = Dimensions.get("window").width;
+let SCREEN_HEIGHT = Dimensions.get("window").height;
 
 var images = [
   { id: 1, src: require("./assets/1.jpg") },
@@ -21,22 +21,136 @@ var images = [
   { id: 4, src: require("./assets/4.jpg") }
 ];
 
-export default function App() {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-    <ScrollView style={{ flex: 1 }}>
-      {images.map(image => {
-        return (
-          <TouchableWithoutFeedback key={image.id}>
-            <Animated.View style={{height: SCREEN_HEIGHT - 150, width: SCREEN_WIDTH, padding: 15}}>
-              <Image source={image.src} style={{flex: 1, height: null, width: null, resizeMode:'cover', borderRadius: 20}}/>
-            </Animated.View>
-          </TouchableWithoutFeedback>
-        );
-      })}
-    </ScrollView>
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      activeImage: null
+    };
+  }
+
+  componentWillMount() {
+    this.allImages = {};
+    this.oldPosition = {};
+    this.position = new Animated.ValueXY();
+    this.dimensions = new Animated.ValueXY();
+  }
+
+  openImage = index => {
+    this.allImages[index].measure((x, y, width, height, pageX, pageY) => {
+      this.oldPosition.x = pageX;
+      this.oldPosition.y = pageY;
+      this.oldPosition.width = width;
+      this.oldPosition.height = height;
+
+      this.position.setValue({
+        x: pageX,
+        y: pageY
+      });
+
+      this.dimensions.setValue({
+        x: width,
+        y: height
+      });
+
+      this.setState({ activeImage: images[index] }, () => {
+        this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+          Animated.parallel([
+            Animated.timing(this.position.x, {
+              toValue: dPageX,
+              duration: 300
+            }),
+            Animated.timing(this.position.y, {
+              toValue: dPageY,
+              duration: 300
+            }),
+            Animated.timing(this.dimensions.x, {
+              toValue: dWidth,
+              duration: 300
+            }),
+            Animated.timing(this.dimensions.y, {
+              toValue: dHeight,
+              duration: 300
+            })
+          ]).start();
+        });
+      });
+    });
+  };
+
+  render() {
+    const activeImageStyle = {
+      width: this.dimensions.x,
+      height: this.dimensions.y,
+      left: this.position.x,
+      top: this.position.y
+    };
+
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          {images.map((image, index) => {
+            return (
+              <TouchableWithoutFeedback
+                key={image.id}
+                onPress={() => this.openImage(index)}
+              >
+                <Animated.View
+                  style={{
+                    height: SCREEN_HEIGHT - 150,
+                    width: SCREEN_WIDTH,
+                    padding: 15
+                  }}
+                >
+                  <Image
+                    ref={image => (this.allImages[index] = image)}
+                    source={image.src}
+                    style={{
+                      flex: 1,
+                      height: null,
+                      width: null,
+                      resizeMode: "cover",
+                      borderRadius: 20
+                    }}
+                  />
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            );
+          })}
+        </ScrollView>
+
+        <View
+          style={StyleSheet.absoluteFill}
+          pointerEvents={this.state.activeImage ? "auto" : "none"}
+        >
+          <View
+            style={{ flex: 2, borderWidth: 1 }}
+            ref={view => {
+              this.viewImage = view;
+            }}
+          >
+            <Animated.Image
+              source={
+                this.state.activeImage ? this.state.activeImage.src : null
+              }
+              style={[
+                {
+                  resizeMode: "cover",
+                  top: 0,
+                  left: 0,
+                  height: null,
+                  width: null
+                },
+                activeImageStyle
+              ]}
+            />
+          </View>
+          <View style={{ flex: 1 }} />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
