3e9cb10c0beb3f0079675bd0e654fe4ed4ec72ab
Matheus Apolonio <matheus.apolonio@hotmail.com>
2019-06-06 01:30:00 -0300

    Implemented the close button'

--- react-native-shared-transition/App.js
+++ react-native-shared-transition/App.js
@@ -4,16 +4,15 @@ import {
   Text,
   View,
   ScrollView,
+  Image,
   TouchableWithoutFeedback,
   Animated,
   Dimensions,
-  Image,
   SafeAreaView
 } from "react-native";
 
 let SCREEN_WIDTH = Dimensions.get("window").width;
 let SCREEN_HEIGHT = Dimensions.get("window").height;
-
 var images = [
   { id: 1, src: require("./assets/1.jpg") },
   { id: 2, src: require("./assets/2.jpg") },
@@ -24,17 +23,17 @@ var images = [
 export default class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
       activeImage: null
     };
   }
-
   componentWillMount() {
     this.allImages = {};
     this.oldPosition = {};
     this.position = new Animated.ValueXY();
     this.dimensions = new Animated.ValueXY();
+    this.animation = new Animated.Value(0);
+    this.activeImageStyle = null;
   }
 
   openImage = index => {
@@ -54,31 +53,67 @@ export default class App extends React.Component {
         y: height
       });
 
-      this.setState({ activeImage: images[index] }, () => {
-        this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
-          Animated.parallel([
-            Animated.timing(this.position.x, {
-              toValue: dPageX,
-              duration: 300
-            }),
-            Animated.timing(this.position.y, {
-              toValue: dPageY,
-              duration: 300
-            }),
-            Animated.timing(this.dimensions.x, {
-              toValue: dWidth,
-              duration: 300
-            }),
-            Animated.timing(this.dimensions.y, {
-              toValue: dHeight,
-              duration: 300
-            })
-          ]).start();
-        });
+      this.setState(
+        {
+          activeImage: images[index]
+        },
+        () => {
+          this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+            Animated.parallel([
+              Animated.timing(this.position.x, {
+                toValue: dPageX,
+                duration: 300
+              }),
+              Animated.timing(this.position.y, {
+                toValue: dPageY,
+                duration: 300
+              }),
+              Animated.timing(this.dimensions.x, {
+                toValue: dWidth,
+                duration: 300
+              }),
+              Animated.timing(this.dimensions.y, {
+                toValue: dHeight,
+                duration: 300
+              }),
+              Animated.timing(this.animation, {
+                toValue: 1,
+                duration: 300
+              })
+            ]).start();
+          });
+        }
+      );
+    });
+  };
+  closeImage = () => {
+    Animated.parallel([
+      Animated.timing(this.position.x, {
+        toValue: this.oldPosition.x,
+        duration: 300
+      }),
+      Animated.timing(this.position.y, {
+        toValue: this.oldPosition.y,
+        duration: 250
+      }),
+      Animated.timing(this.dimensions.x, {
+        toValue: this.oldPosition.width,
+        duration: 250
+      }),
+      Animated.timing(this.dimensions.y, {
+        toValue: this.oldPosition.height,
+        duration: 250
+      }),
+      Animated.timing(this.animation, {
+        toValue: 0,
+        duration: 250
+      })
+    ]).start(() => {
+      this.setState({
+        activeImage: null
       });
     });
   };
-
   render() {
     const activeImageStyle = {
       width: this.dimensions.x,
@@ -87,14 +122,37 @@ export default class App extends React.Component {
       top: this.position.y
     };
 
+    const animatedContentY = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-150, 0]
+    });
+
+    const animatedContentOpacity = this.animation.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 1, 1]
+    });
+
+    const animatedContentStyle = {
+      opacity: animatedContentOpacity,
+      transform: [
+        {
+          translateY: animatedContentY
+        }
+      ]
+    };
+
+    const animatedCrossOpacity = {
+      opacity: this.animation
+    };
+
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <ScrollView style={{ flex: 1 }}>
           {images.map((image, index) => {
             return (
               <TouchableWithoutFeedback
-                key={image.id}
                 onPress={() => this.openImage(index)}
+                key={image.id}
               >
                 <Animated.View
                   style={{
@@ -119,16 +177,13 @@ export default class App extends React.Component {
             );
           })}
         </ScrollView>
-
         <View
           style={StyleSheet.absoluteFill}
           pointerEvents={this.state.activeImage ? "auto" : "none"}
         >
           <View
-            style={{ flex: 2, borderWidth: 1 }}
-            ref={view => {
-              this.viewImage = view;
-            }}
+            style={{ flex: 2, zIndex: 1001 }}
+            ref={view => (this.viewImage = view)}
           >
             <Animated.Image
               source={
@@ -145,19 +200,48 @@ export default class App extends React.Component {
                 activeImageStyle
               ]}
             />
+            <TouchableWithoutFeedback onPress={() => this.closeImage()}>
+              <Animated.View
+                style={[
+                  { position: "absolute", top: 30, right: 30 },
+                  animatedCrossOpacity
+                ]}
+              >
+                <Text
+                  style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
+                >
+                  X
+                </Text>
+              </Animated.View>
+            </TouchableWithoutFeedback>
           </View>
-          <View style={{ flex: 1 }} />
+          <Animated.View
+            style={[
+              {
+                flex: 1,
+                zIndex: 1000,
+                backgroundColor: "white",
+                padding: 20,
+                paddingTop: 50
+              },
+              animatedContentStyle
+            ]}
+          >
+            <Text style={{ fontSize: 24, paddingBottom: 10 }}>Lorem ipsum</Text>
+            <Text>
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised
+              in the 1960s with the release of Letraset sheets containing Lorem
+              Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </Text>
+          </Animated.View>
         </View>
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
